9ebf853c0b02119ccea11b6ee220dd60621e6763
Liam Harbec <liam.harbec@gmail.com>
2021-09-16 11:55:50 -0700

    Added guessing help

--- exercise.rb
+++ exercise.rb
@@ -1,6 +1,8 @@
 #Solo Git Exercise for COSC499
 #This exercise will contain a guessing game.
 
+#In this branch, the game will assist the user by telling them if they need to guess higher, or lower.
+
 
 num = (rand() * 10).to_i
 puts "Welcome to the guessing game! I'm thinking of a number between 1 and 10"
@@ -11,6 +13,10 @@ while (userResponse.to_i != num)
   if userResponse.to_i == num
     puts "You guessed it!"
   else
-    puts "Try again!"
+    if userResponse.to_i > num
+      puts "Guess lower"
+    else 
+      puts "Guess Higher"
+    end
   end
 end
