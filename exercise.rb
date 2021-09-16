f19e418f5fe6ab3965b85a28b40052501b2149e0
Liam Harbec <liam.harbec@gmail.com>
2021-09-16 11:50:47 -0700

    Added base game

--- exercise.rb
+++ exercise.rb
@@ -1,1 +1,16 @@
 #Solo Git Exercise for COSC499
+#This exercise will contain a guessing game.
+
+
+num = (rand() * 10).to_i
+puts "Welcome to the guessing game! I'm thinking of a number between 1 and 10"
+userResponse = ""
+
+while (userResponse.to_i != num)
+  userResponse = gets
+  if userResponse.to_i == num
+    puts "You guessed it!"
+  else
+    puts "Try again!"
+  end
+end
